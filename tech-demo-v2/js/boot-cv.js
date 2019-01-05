0ca0e8d8f9e7bb29d36eb9e7b9566487bdc98959
Noah Levenson <noahlevenson@gmail.com>
2019-01-04 20:18:59 -0500

    Revert to ssdnet

--- tech-demo-v2/js/boot-cv.js
+++ tech-demo-v2/js/boot-cv.js
@@ -17,8 +17,8 @@ const bootCVState = {
 			}
 		}, 500);
 
-		const detectorModel = faceapi.loadTinyFaceDetectorModel("../tech-demo-v2/lib/models")
-		// faceapi.loadSsdMobilenetv1Model("../tech-demo-v2/lib/models")
+		const detectorModel = faceapi.loadSsdMobilenetv1Model("../tech-demo-v2/lib/models")
+		// faceapi.loadTinyFaceDetectorModel("../tech-demo-v2/lib/models")
 
 		detectorModel.then(() => {
 			initText.text = "Initializing assets 2/3";
